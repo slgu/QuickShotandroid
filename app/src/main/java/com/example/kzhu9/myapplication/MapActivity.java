f3261cce156dac0fe95f6cb5ff77803f6cdc11b9
j20120307 <j20120307@gmail.com>
2015-12-22 22:31:43 -0500

    Fix bugs, code reformated

--- app/src/main/java/com/example/kzhu9/myapplication/MapActivity.java
+++ app/src/main/java/com/example/kzhu9/myapplication/MapActivity.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.example.kzhu9.fragments.MapViewFragment;
 
@@ -31,5 +32,22 @@ public class MapActivity extends AppCompatActivity {
        fragment.setArguments(bundle);
        FragmentManager fm = getSupportFragmentManager();
        fm.beginTransaction().add(R.id.container, fragment, null).commit();
+
    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
 }
